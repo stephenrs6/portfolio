c71fddba88394f3e8c7b609a09bd5b8b260474a4
stephen <stephenrs7@gmail.com>
2018-03-12 18:43:34 -0700

    third commit

--- src/components/Contents/Contents.js
+++ src/components/Contents/Contents.js
@@ -3,8 +3,15 @@ import "./Contents.css";
 
 const Contents = (props) => (
     <div className="container contents">
-        <h3>About Me:</h3>
-        <p className="flow-text">I am a technology enthusiast and creative problem solver who is currently immersed in the art of Web Development. I'm able to build complex websites with full database back-end all the way from mock-up to roll-out. I have recently completed UC San Diego Extension's Full Stack Web Development program and aim to obtain a junior Web Development position. I obtained my Bachelor's in Psychology from UC San Diego, making me a dynamic collaborator knowledgable of the human condition, and adept at working in diverse teams. I'm also proficient in Mandarin Chinese at a native speaker level.</p>
+        <h4>About Me:</h4>
+        <p className="flow-text">I am a technology enthusiast and creative problem solver who is currently immersed in the art of Web Development. 
+        I utilize my knowledge of human-computer interactions to design User Interfaces that come intuitively for the end user.
+        I'm able to build complex websites with full database back-end all the way from mock-up to roll-out. 
+        <br></br>
+        <br></br>
+        I have recently completed UC San Diego Extension's Full Stack Web Development program and aim to obtain a junior Web Developer position. 
+        I obtained my Bachelor's in Psychology from UC San Diego, making me a dynamic collaborator knowledgable of the human condition, and adept at working in diverse teams. 
+        I'm also proficient in Mandarin Chinese at a native speaker level.</p>
     </div>
 );
 
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,13 @@
 import React from "react";
-// import "./Footer.css";
+import "./Footer.css";
 
 const Footer = (props) => (
     <div className="footer">
     
-<p>Footer goes here</p>
+<p>Connect with me:</p>
+  <a href="https://www.linkedin.com/in/yaotesung/"><img src="/assets/images/linkedin.png" alt="LinkedIn"></img></a>
+  <a href="https://github.com/stephenrs6"><img src="/assets/images/github.png" alt="GitHub"></img></a>
+  <a href="mailto:stephenrs7@gmail.com"><img src="/assets/images/gmail.png" alt="Email"></img></a>
   </div>
 );
 

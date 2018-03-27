3153f904cf3d112831aae3e0fa71038a4d3fbd80
stephen <stephenrs7@gmail.com>
2018-03-27 03:30:56 -0700

    customize

--- src/components/Contents/Interests.js
+++ src/components/Contents/Interests.js
@@ -11,10 +11,10 @@ import { Row, Col } from "react-materialize";
 // const userID = "5a8624f2fe9f971c5c565bb3";
 
 class Interests extends Component {
-    state = {
-        result: [],
-        search: ""
-    };
+    // state = {
+    //     result: [],
+    //     search: ""
+    // };
     // componentDidMount() {
     //     this.getBioData(userID);
     // };
@@ -28,7 +28,17 @@ class Interests extends Component {
     render() {
         return (
             <div className="potentials-list">
-                <p>interests</p>
+            <div className="container contents">
+                <h4>About Me:</h4>
+                <p className="flow-text">I am a technology enthusiast and creative problem solver who is currently immersed in the art of Web Development.
+                I utilize my knowledge of human-computer interactions to design User Interfaces that come intuitively for the end user.
+                I'm able to build complex websites with full database back-end all the way from mock-up to roll-out.
+                    <br></br>
+                    <br></br>
+                I have recently completed UC San Diego Extension's Full Stack Web Development program and aim to obtain a junior Web Developer position.
+                I obtained my Bachelor's in Psychology from UC San Diego, making me a dynamic collaborator knowledgable of the human condition, and adept at working in diverse teams.
+                I'm also proficient in Mandarin Chinese at a native speaker level.</p>
+            </div>
             </div>
             )
 

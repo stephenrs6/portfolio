a437273105a380c67f0a742d191050cf0524d7ff
Stephen <stephenrs7@gmail.com>
2018-04-27 09:09:12 -0800

    updates

--- src/components/Contents/Contents.js
+++ src/components/Contents/Contents.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, NavLink, HashRouter } from "react-router-dom";
+import {Route, NavLink, BrowserRouter as Router} from "react-router-dom";
 import "./Contents.css";
 import About from "./About";
 import Skills from "./Skills";
@@ -8,37 +8,37 @@ import Interests from "./Interests";
 import Resume from "./Resume";
 
 const Contents = (props) => (
-<HashRouter>
-            <div>
-                <div className ="navbar">
-                    <ul>
-                        <li>
-                            <NavLink to="/about">ABOUT</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/skills">SKILLS</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/projects">PROJECTS</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/interests">INTERESTS</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/resume">RESUME</NavLink>
-                        </li>
-                    </ul>
-                </div>
-                <div className="private-content">
-                    {/* <Route path="/" component={Placeholder} /> */}
-                    <Route path="/about" component={About} />
-                    <Route path="/skills" component={Skills} />
-                    <Route path="/projects" component={Projects} />
-                    <Route path="/interests" component={Interests} />
-                    <Route path="/resume" component={Resume} />
-                </div>
-            </div>
-        </HashRouter>
+  <Router>
+    <div>
+      <div className="navbar">
+        <ul>
+          <li>
+            <NavLink to="/about">ABOUT</NavLink>
+          </li>
+          <li>
+            <NavLink to="/skills">SKILLS</NavLink>
+          </li>
+          <li>
+            <NavLink to="/projects">PROJECTS</NavLink>
+          </li>
+          <li>
+            <NavLink to="/interests">INTERESTS</NavLink>
+          </li>
+          <li>
+            <NavLink to="/resume">RESUME</NavLink>
+          </li>
+        </ul>
+      </div>
+      <div className="private-content">
+        <Route exact path="/" component={About}/>
+        <Route exact path="/about" component={About}/>
+        <Route exact path="/skills" component={Skills}/>
+        <Route exact path="/projects" component={Projects}/>
+        <Route exact path="/interests" component={Interests}/>
+        <Route exact path="/resume" component={Resume}/>
+      </div>
+    </div>
+  </Router>
 );
 
 export default Contents;
